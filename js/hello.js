cb2794dbce55b5fbe085f19822f0bc602c91038e
gailo22 <gailo22@gmail.com>
2012-03-10 22:49:06 +0800

    added js files

---
+++ js/hello.js
@@ -0,0 +1,27 @@
+var hello = function() {
+alert("hello world");
+}
+
+hello();
+
+
+
+var hello2 = function(element, function() {
+
+alert("hello world2");
+
+});
+
+
+var hello3 = function(element, function() {
+alert("there are many situation there are many situation there");
+})
+
+hello();
+hello();
+hello();
+hello();
+hello();
+hello();
+hello();
+hello();
